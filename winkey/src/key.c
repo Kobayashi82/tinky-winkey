31609dda95c40d4a0a54fe385671dfaa40b7596b
Kobayashi82 <kobayashi82@outlook.com>
2025-08-17 16:37:35 +0200

    Añadidas algunas teclas y [ALTGR]

--- winkey/src/key.c
+++ winkey/src/key.c
@@ -14,31 +14,61 @@ static char keyStringBuffer[32];
 static void GetActiveModifiers(char* dst, size_t cap)
 {
     dst[0] = '\0';
-    if (GetAsyncKeyState(VK_CONTROL) & 0x8000) strcat_s(dst, cap, "[CTRL]");
-    if (GetAsyncKeyState(VK_MENU)    & 0x8000) strcat_s(dst, cap, "[ALT]");
-    if (GetAsyncKeyState(VK_SHIFT)   & 0x8000) strcat_s(dst, cap, "[SHIFT]");
+    if (GetAsyncKeyState(VK_RCONTROL) & 0x8000) strcat_s(dst, cap, "[RCTRL]");
+    if (GetAsyncKeyState(VK_LSHIFT) & 0x8000)   strcat_s(dst, cap, "[LSHIFT]");
+    if (GetAsyncKeyState(VK_RSHIFT) & 0x8000)   strcat_s(dst, cap, "[RSHIFT]");
+	if (GetAsyncKeyState(VK_LMENU) & 0x8000)    strcat_s(dst, cap, "[LALT]");
+    if ((GetAsyncKeyState(VK_LCONTROL) & 0x8000) && (GetAsyncKeyState(VK_RMENU) & 0x8000))
+		strcat_s(dst, cap, "[ALTGR]");
+	else {
+    	if (GetAsyncKeyState(VK_LCONTROL) & 0x8000) strcat_s(dst, cap, "[LCTRL]");
+		if (GetAsyncKeyState(VK_RMENU) & 0x8000)    strcat_s(dst, cap, "[RALT]");
+	}
 }
 
 // Maps special keys to readable labels
 static const char* SpecialKeyToString(DWORD vk)
 {
     switch (vk) {
-        case VK_RETURN:  return "\n";
-        case VK_BACK:    return "[BACKSPACE]";
-        case VK_TAB:     return "[TAB]";
-        case VK_ESCAPE:  return "[ESC]";
-        case VK_CAPITAL: return "[CAPS]";
-        case VK_INSERT:  return "[INSERT]";
-        case VK_DELETE:  return "[DELETE]";
-        case VK_HOME:    return "[HOME]";
-        case VK_END:     return "[END]";
-        case VK_PRIOR:   return "[PAGE UP]";
-        case VK_NEXT:    return "[PAGE DOWN]";
-        case VK_LEFT:    return "[LEFT]";
-        case VK_RIGHT:   return "[RIGHT]";
-        case VK_UP:      return "[UP]";
-        case VK_DOWN:    return "[DOWN]";
-        default:         return NULL;
+        case VK_RETURN:              return "\n";
+        case VK_BACK:                return "[BACKSPACE]";
+        case VK_TAB:                 return "[TAB]";
+        case VK_ESCAPE:              return "[ESC]";
+        case VK_CAPITAL:             return "[CAPS]";
+        case VK_INSERT:              return "[INSERT]";
+        case VK_DELETE:              return "[DELETE]";
+        case VK_HOME:                return "[HOME]";
+        case VK_END:                 return "[END]";
+        case VK_PRIOR:               return "[PAGE UP]";
+        case VK_NEXT:                return "[PAGE DOWN]";
+        case VK_LEFT:                return "[LEFT]";
+        case VK_RIGHT:               return "[RIGHT]";
+        case VK_UP:                  return "[UP]";
+        case VK_DOWN:                return "[DOWN]";
+        case VK_F1:                  return "[F1]";
+        case VK_F2:                  return "[F2]";
+        case VK_F3:                  return "[F3]";
+        case VK_F4:                  return "[F4]";
+        case VK_F5:                  return "[F5]";
+        case VK_F6:                  return "[F6]";
+        case VK_F7:                  return "[F7]";
+        case VK_F8:                  return "[F8]";
+        case VK_F9:                  return "[F9]";
+        case VK_F10:                 return "[F10]";
+        case VK_F11:                 return "[F11]";
+        case VK_F12:                 return "[F12]";
+        case VK_VOLUME_MUTE:         return "[VOLUME MUTE]";
+        case VK_VOLUME_DOWN:         return "[VOLUME DOWN]";
+        case VK_VOLUME_UP:           return "[VOLUME UP]";
+        case VK_MEDIA_NEXT_TRACK:    return "[MEDIA NEXT]";
+        case VK_MEDIA_PREV_TRACK:    return "[MEDIA PREV]";
+        case VK_MEDIA_STOP:          return "[MEDIA STOP]";
+        case VK_MEDIA_PLAY_PAUSE:    return "[MEDIA PLAY/PAUSE]";
+        case VK_LAUNCH_MAIL:         return "[MAIL]";
+        case VK_LAUNCH_MEDIA_SELECT: return "[MEDIA SELECT]";
+        case VK_LAUNCH_APP1:         return "[APP1]";
+        case VK_LAUNCH_APP2:         return "[APP2]";
+        default:                     return NULL;
     }
 }
 
